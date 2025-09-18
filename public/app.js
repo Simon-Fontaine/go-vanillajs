a3f411f1f085e25a50fbe3e0a7651788e49dc72b
Simon Fontaine <github@simonfontaine.com>
2025-09-18 18:03:48 +0200

    feat: added basic javascript with api service

---
+++ public/app.js
@@ -0,0 +1,9 @@
+import API from "./services/API.js";
+
+window.app = {
+  search: (event) => {
+    event.preventDefault();
+    const query = document.querySelector("input[type=search]").value;
+  },
+  api: API,
+};
